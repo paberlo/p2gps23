6b9ad5251cd1fe5fce918db5abd333bfa9b28b68
paberlo <pablo.bermejo@uclm.es>
2023-10-03 09:44:37 +0200

    Dockerfiles configurados

--- deploy/docker/Order/drop/run.sh
+++ deploy/docker/Order/drop/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#"mongo" is the name of the container running MongoDB, indicado también en el archivo application.properties
 while ! curl http://mongo:27017/
 do
   echo "$(date) - still trying"
@@ -7,4 +8,4 @@ do
 done
 echo "$(date) - connected successfully"
 
-java -jar ordering-*.jar
+java -jar usr/local/app/ordering-service-0.1.0.jar -spring.config.location=application.properties 
